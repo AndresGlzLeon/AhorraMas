cb003f575299dafd9a2930cbf4fe645afc114a1d
DiegoCruz-Cruz <124048797@upq.edu.mx>
2025-11-02 19:38:58 -0600

    Interfaz de Ingresos y Egresos completada, adjuntando imágenes usadas

--- Proyecto/screens/IngresosEgresos.js
+++ Proyecto/screens/IngresosEgresos.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, Image} from "react-native";
+import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
 
 export default function IngresosEgresos() {
   return (
     <View style={styles.container}>
+
       <View style={styles.header}>
         <View style={styles.leftIcons}>
           <Image source={require("../assets/ajustes.png")} style={styles.iconHeader} />
-
           <Image source={require("../assets/notificaciones.png")} style={[styles.iconHeader, { marginLeft: 10 }]} />
         </View>
 
@@ -19,52 +19,97 @@ export default function IngresosEgresos() {
       </View>
 
       <ScrollView contentContainerStyle={styles.scrollContent}>
+        
         <View style={styles.headerSection}>
           <View>
-            <Text style={styles.welcome}>Gastos,{"\n"}Programados</Text>
+            <Text style={styles.welcome}>Ingresos &{"\n"}Egresos</Text>
           </View>
+
           <Image source={require("../assets/logo.png")} style={styles.pigImage} />
         </View>
 
-        <Text style={styles.sectionTitle}>Administra tus pagos</Text>
+        <View style={styles.graphContainer}>
+          <Image 
+            source={require("../assets/grafica.png")} 
+            style={styles.graphImage} 
+          />
+        </View>
+
+        <View style={styles.infoCard}>
+          <Image 
+            source={require("../assets/sueldo.png")} 
+            style={styles.smallIcon}
+          />
+
+          <View style={{ marginLeft: 15 }}>
+            <Text style={styles.infoTitle}>Ingresos</Text>
+            <Text style={styles.infoAmount}>+$1000.0</Text>
+          </View>
+        </View>
+
+        <View style={styles.infoCard}>
+          <Image 
+            source={require("../assets/sueldo.png")} 
+            style={styles.smallIcon}
+          />
+
+          <View style={{ marginLeft: 15 }}>
+            <Text style={styles.infoTitle}>Ingresos</Text>
+            <Text style={styles.infoAmount}>+$300.0</Text>
+          </View>
+        </View>
+
+        <View style={styles.infoCard}>
+          <Image 
+            source={require("../assets/sueldoBajo.png")} 
+            style={styles.smallIcon}
+          />
+
+          <View style={{ marginLeft: 15 }}>
+            <Text style={styles.infoTitle}>Egresos</Text>
+            <Text style={[styles.infoAmount, { color: "red" }]}>-$230.0</Text>
+          </View>
+
+        </View>
+
+
 
       </ScrollView>
 
-        <View style={styles.bottomNav}>
+      <View style={styles.bottomNav}>
 
         <View style={styles.iconCircle}>
-        <Image source={require("../assets/Transisiones.png")} style={styles.navIcon} />
+          <Image source={require("../assets/Pink.png")} style={styles.navIcon} />
         </View>
 
         <View style={styles.iconCircle}>
-        <Image source={require("../assets/Pink.png")} style={styles.navIcon} />
+          <Image source={require("../assets/BolsaDinero.png")} style={styles.navIcon} />
         </View>
 
         <View style={styles.centerButton}>
-        <Image source={require("../assets/Programados.png")} style={styles.centerIcon} />
+          <Image source={require("../assets/Transisiones.png")} style={styles.centerIcon} />
         </View>
 
         <View style={styles.iconCircle}>
-        <Image source={require("../assets/inicio.png")} style={styles.navIcon} />
+          <Image source={require("../assets/inicio.png")} style={styles.navIcon} />
         </View>
 
         <View style={styles.iconCircle}>
-        <Image source={require("../assets/BolsaDinero.png")} style={styles.navIcon} />
+          <Image source={require("../assets/Programados.png")} style={styles.navIcon} />
         </View>
+
+      </View>
 
     </View>
-
-</View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
   container: { 
     flex: 1,
-   backgroundColor: "#fff",
-   alignItems:"center",
-
-},
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
 
   header: {
     flexDirection: "row",
@@ -73,46 +118,49 @@ const styles = StyleSheet.create({
     padding: 15,
     backgroundColor: "#f4f1ff",
     borderRadius: 40,
-    width: " 95%",
+    width: "95%",
     marginTop: 50,
-    
-    
   },
-  leftIcons: {
-    flexDirection: "row", 
-    alignItems: "center" },
 
-  iconHeader: { 
+  leftIcons: { flexDirection: "row", alignItems: "center" },
+
+  iconHeader: {
     width: 33,
-    height: 22, 
-    resizeMode: "contain" },
+    height: 22,
+    resizeMode: "contain",
+  },
 
-  title: { 
-    fontSize: 18, 
-    fontWeight: "600", 
-    color: "#333" },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#333",
+  },
 
   avatar: {
     backgroundColor: "#b3a5ff",
     borderRadius: 50,
     padding: 8,
   },
+
   avatarIcon: {
     width: 20,
-    height: 20, 
+    height: 20,
     tintColor: "#fff",
-    resizeMode: "contain" },
+    resizeMode: "contain",
+  },
 
-  scrollContent: { 
+  scrollContent: {
     padding: 20,
-    paddingBottom: 120 },
-    
+    paddingBottom: 120,
+  },
+
   headerSection: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 20,
   },
+
   welcome: {
     fontSize: 26,
     paddingRight: 100,
@@ -120,19 +168,61 @@ const styles = StyleSheet.create({
     color: "#7b6cff",
     lineHeight: 30,
   },
-    pigImage: {
-        width: 80,
-        height: 80,
-        resizeMode: "contain",
-    },
+
+  pigImage: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain",
+  },
+
+  graphContainer: {
+    width: "100%",
+    backgroundColor: "#f7f2ff",
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  graphImage: {
+    width: "90%",
+    height: 180,
+    resizeMode: "contain",
+  },
+
+  infoCard: {
+    backgroundColor: "#f7f2ff",
+    padding: 15,
+    borderRadius: 20,
+    marginBottom: 15,
+    width: "100%",
+    flexDirection: "row", 
+    alignItems: "center",
+  },
+
+  smallIcon: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain",
+  },
 
 
+  infoTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#6a5acd",
+  },
 
+  infoAmount: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: "green",
+    marginTop: 5,
+  },
 
   bottomNav: {
     position: "absolute",
     bottom: 10,
-
     width: "95%",
     flexDirection: "row",
     justifyContent: "space-around",
@@ -140,33 +230,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#eae2ff",
     paddingVertical: 12,
     borderRadius: 30,
-    
   },
+
   iconCircle: {
-    width: 50,       
+    width: 50,
     height: 50,
-    borderRadius: 25, 
-    backgroundColor: "#A084E8", 
+    borderRadius: 25,
+    backgroundColor: "#A084E8",
     justifyContent: "center",
     alignItems: "center",
-    marginHorizontal: 5, 
   },
-  
-  navIcon: { 
-    width: 26, 
-    height: 26, 
-    resizeMode: "contain" },
+
+  navIcon: {
+    width: 26,
+    height: 26,
+    resizeMode: "contain",
+  },
 
   centerButton: {
     backgroundColor: "#7f6aff",
     padding: 15,
     borderRadius: 40,
     marginBottom: 25,
-
   },
+
   centerIcon: {
-    width: 30, 
-    height: 30, 
-    resizeMode: "contain", 
-    tintColor: "#fff" },
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
+    tintColor: "#fff",
+  },
 });
