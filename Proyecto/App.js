fc605c939d96ffd35a5eb95d33c16412a1dc0afe
Vannys19 <124051404@upq.edu.mx>
2025-11-21 18:06:49 -0600

    Barra inferior con Tab.navigation

--- Proyecto/App.js
+++ Proyecto/App.js
@@ -1,16 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Principal from './screens/Principal';
-import MenusScreens from './screens/MenusScreens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import 'react-native-get-random-values';
+import 'react-native-gesture-handler';
 
+import Principal from './screens/Principal';
+import PagosProgramados from './screens/PagosProgramados';
+import Presupuesto from './screens/Presupuesto';
+import IngresosEgresos from './screens/IngresosEgresos';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <MenusScreens/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Principal"
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Principal') {
+              iconName = 'home';
+            } else if (route.name === 'PagosProgramados') {
+              iconName = 'calendar';
+            } else if (route.name === 'Presupuesto') {
+              iconName = 'pie-chart';
+            } else if (route.name === 'IngresosEgresos') {
+              iconName = 'wallet';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#007BFF',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            paddingBottom: 5,
+            height: 60,
+          },
+        })}
+      >
+        <Tab.Screen name="Principal" component={Principal} />
+        <Tab.Screen name="PagosProgramados" component={PagosProgramados} />
+        <Tab.Screen name="Presupuesto" component={Presupuesto} />
+        <Tab.Screen name="IngresosEgresos" component={IngresosEgresos} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -22,7 +59,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-
-
