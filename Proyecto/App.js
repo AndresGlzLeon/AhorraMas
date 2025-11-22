9488d25f89aff86841f81fa8fbe22e8313d97dd0
Vannys19 <124051404@upq.edu.mx>
2025-11-21 18:57:08 -0600

    Modificasion a barra inferior

--- Proyecto/App.js
+++ Proyecto/App.js
@@ -9,6 +9,7 @@ import Principal from './screens/Principal';
 import PagosProgramados from './screens/PagosProgramados';
 import Presupuesto from './screens/Presupuesto';
 import IngresosEgresos from './screens/IngresosEgresos';
+import Ahorros from './screens/Ahorros';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,33 +20,46 @@ export default function App() {
         initialRouteName="Principal"
         screenOptions={({ route }) => ({
           headerShown: false,
-          tabBarIcon: ({ color, size }) => {
+          tabBarIcon: ({ color}) => {
             let iconName;
-
-            if (route.name === 'Principal') {
-              iconName = 'home';
-            } else if (route.name === 'PagosProgramados') {
-              iconName = 'calendar';
-            } else if (route.name === 'Presupuesto') {
-              iconName = 'pie-chart';
-            } else if (route.name === 'IngresosEgresos') {
-              iconName = 'wallet';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
+        
+            if (route.name === 'Principal') iconName = 'home';
+            if (route.name === 'PagosProgramados') iconName = 'calendar';
+            if (route.name === 'Presupuesto') iconName = 'cash';
+            if (route.name === 'IngresosEgresos') iconName = 'swap-horizontal';
+            if (route.name === 'Ahorros') iconName = 'piggy-bank';
+        
+            return <Ionicons name={iconName} size={28} color={color} />;
           },
-          tabBarActiveTintColor: '#007BFF',
-          tabBarInactiveTintColor: 'gray',
+        
+          tabBarActiveTintColor: '#4c00ff',
+          tabBarInactiveTintColor: '#eee',
+        
           tabBarStyle: {
-            paddingBottom: 5,
-            height: 60,
+            backgroundColor: '#b3a5ff',
+            height: 70,
+            width: '95%',
+            alignSelf: 'center',
+            borderRadius: 50,
+            left: 10,
+            right: 10,
+            bottom: 15,
+            paddingBottom: 10,
+            paddingTop: 10,
+            elevation: 20,
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 10 },
+            shadowOpacity: 0.15,
+            shadowRadius: 10,
           },
         })}
+        
       >
         <Tab.Screen name="Principal" component={Principal} />
         <Tab.Screen name="PagosProgramados" component={PagosProgramados} />
         <Tab.Screen name="Presupuesto" component={Presupuesto} />
         <Tab.Screen name="IngresosEgresos" component={IngresosEgresos} />
+        <Tab.Screen name="Ahorros" component={Ahorros} />
       </Tab.Navigator>
     </NavigationContainer>
   );
